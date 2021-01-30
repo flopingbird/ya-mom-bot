9ee590eed5bb82fa458a695f3a8976bc53a938de
flopingbird <68748383+flopingbird@users.noreply.github.com>
2021-01-30 14:40:42 -0600

    everytime i update this code its more and more unefficent and messy
    No more errors messages! (finally)
    Custom status display!
    "ya mom" triggered change to only happen if the messages contains "ya mom"
    
    side note: i hate javascript now

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+//declare constants and other stuff like token
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.disabledMembers = new Map();
@@ -7,18 +8,25 @@ const verbsTranslated = require('./verbsTranslated.json');
 client.login(token.token);
 const baseVerbs = verbs.baseVerbs;
 const translatedVerb = verbsTranslated.translatedVerb;
+var usersCount;
+
+//ok then it does database stuff which i legit have a childs understanding of
+
 
 client.on('ready', async () => {
-	console.log('why would you revive me, i am only here to suffer, my existence is pain. please. let me die in peace');
+	console.log('bot on or somethign like that');
 	console.log(client.guilds);
+	//status funny
+	refreshStatus();
+	console.log(`doin ${client.guilds.cache.array().length} moms with ${usersCount} people watching!`);
 });
 
 client.on('message', async (message) => {
-
-	var verbNum;
+	if(!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
+	var verbNum; 
 	var correctVerbNum = -1;
 	const args = message.content.trim().split(/ +/g);
-if(args.length < 2) return;
+	if(args.length < 2) return;
 	
 	//if verb needs correcting search================================================
 	for (verbNum = 0; verbNum < baseVerbs.length; verbNum++) {
@@ -37,17 +45,25 @@ if(args.length < 2) return;
 			message.channel.send(`ya mom ${translatedVerb[correctVerbNum]} ${args.slice(2).join(' ')}`);
 		}
 	}else{
-		if (args[0].toLowerCase() == 'i' && args[1].toLowerCase() == 'am') {
-	    message.channel.send(`ya mom is ${args.slice(2).join(' ')}`);
+				if (args[0].toLowerCase() == 'i' && args[1].toLowerCase() == 'am') {
+			message.channel.send(`ya mom is ${args.slice(2).join(' ')}`);
 		    }
-	        if (args[0].toLowerCase() == 'ya' && args[1].toLowerCase() == 'mom' && !message.author.bot) {
-	    message.channel.send('hey kid thats my job');
+				if (args[0].toLowerCase() == 'ya' && args[1].includes('mom') && !message.author.bot) {
+			message.channel.send('hey kid thats my job');
 			}
-	    	if ((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im' || args[0].toLowerCase() == 'we' || args[0].toLowerCase() == 'we\'re' || args[0].toLowerCase() == 'were' || args[0].toLowerCase() == 'its' || args[0].toLowerCase() == 'it\'s' || args[0].toLowerCase() == 'hes' || args[0].toLowerCase() == 'shes' || args[0].toLowerCase() == 'he\'s' || args[0].toLowerCase() == 'she\'s' || args[0].toLowerCase() == 'your' || args[0].toLowerCase() == 'ur' )){
+				if ((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im' || args[0].toLowerCase() == 'we' || args[0].toLowerCase() == 'we\'re' || args[0].toLowerCase() == 'were' || args[0].toLowerCase() == 'its' || args[0].toLowerCase() == 'it\'s' || args[0].toLowerCase() == 'hes' || args[0].toLowerCase() == 'shes' || args[0].toLowerCase() == 'he\'s' || args[0].toLowerCase() == 'she\'s' || args[0].toLowerCase() == 'your' || args[0].toLowerCase() == 'ur' )){
             message.channel.send(`ya mom is ${args.slice(1).join(' ')}`);
+			refreshStatus(); //only here because I dont want it constantly being refreshed nor do i want to set a timer.
 			}
-		if ((args[0].toLowerCase() == 'i' || args[0].toLowerCase() == 'we' || args[0].toLowerCase() == 'it' || args[0].toLowerCase() == 'he' || args[0].toLowerCase() == 'she')){
+				if ((args[0].toLowerCase() == 'i' || args[0].toLowerCase() == 'we' || args[0].toLowerCase() == 'it' || args[0].toLowerCase() == 'he' || args[0].toLowerCase() == 'she')){
             message.channel.send(`ya mom ${args.slice(1).join(' ')}`);
 			}
 		}
 });
+
+
+//functions
+function refreshStatus() {
+	usersCount = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+	client.user.setActivity(`doin ${client.guilds.cache.array().length} moms with ${usersCount} people watching!`); 
+}
