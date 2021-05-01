68b133ab668fd9a13cea334e1c01a7f00c49cc63
flopingbird <68748383+flopingbird@users.noreply.github.com>
2021-04-30 20:20:39 -0500

    v1.3 crashes, efficiency, and /commands
    -patched known crash issues (finally)
    -cleaned up large portion of orginal code (not all of it though)
    Made things MUCH more efficent:
       -added cases instead of having several if elses for commands
       -redid functions from like 15 years ago
       - status now refreshes at time interval instead of guild joins and leaves
    -finally got around to intergrating slash commands into ya mom bot, meaing you can do use "/help" instead of "@ya mom bot#2891 help"
    -reworded some commands info and results
    -fixed issue with commands being case sensitive

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const embded = new Discord.MessageEmbed() //no embeds? im outta here!
 	.addFields(
 		{ name: 'help', value: 'this command' },
 		{ name: 'donate', value: 'sends donation link' },
-		{ name: 'FAQ', value: 'sends FAQ' },
+		{ name: 'FAQ', value: 'sends FAQ\'s' },
 		{ name: 'server', value: 'sends invite link for support server' },
 		{ name: 'severcount', value: 'sends amount of servers ya mom bot is in' },
 	)
@@ -30,60 +30,55 @@ const embded = new Discord.MessageEmbed() //no embeds? im outta here!
 const ap = AutoPoster(tokens.apToken, client);
 ap.on('posted', () => {
 	console.log('updated')
+	client.user.setActivity(`with @ya mom bot#7927 (imposter) | @ya mom bot help`, { type: "PLAYING"}); //i am way to proud of myself for putting this here
 })
 
-//================================================================================================================================================================================
+//===============================================================
 
 client.on('ready', async () => {
 	console.log('bot on or somethign like that');
 	//status funny
-	refreshStatus();
+	client.user.setActivity(`with @ya mom bot#7927 (imposter) | @ya mom bot help`, { type: "PLAYING"}); //i am way to proud of myself for putting this here
 });
 
 
 //messages =====
 client.on('message', async (message) => {
 	if (message.channel.type === 'dm' || message.author.bot || !message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
-	const args = message.content.trim().split(/ +/g); 
-	if(args.slice(0).join(" ").length >= 1985) return; //BRO I HAVE NO FUCKING CLUE WHY
-	//comands ==========  							I CANT JUST DO message.content.length
+	const args = message.content.trim().split(/ +/g);
+	if(args.slice(0).join(" ").length >= 1985 || args.length < 2) return; //| BRO I HAVE NO FUCKING CLUE WHY | just realized this looks alot neater with it divided
+	//commands section (i shoudl probally make this a secdons .js or soemthing) | I CANT JUST DO message.content.length |
 	if(args[0] == "<@!743110495992807495>" || args[0] == "<@743110495992807495>"){
-		if(args[1] == "help") { //help command
-			message.channel.send(embded);
-		}//timne to checj through all the possibnle commands !  ! :DDDDD
-		else if(args[1] == "donate") {
-			message.channel.send("thank you for considering donating to this ~~horrible~~ great bot!\nhttps://www.patreon.com/yamombot");
-		}else if(args[1] == "website") {
-			message.channel.send("thank you for considering giving me ad sens\nhttps://yamombot.info");
-		}else if(args[1] == "FAQ") {
-			message.channel.send("no, i am not gonna add a toggle in the next month or two");
-		}else if (args[1] == "server"){
-			message.channel.send("https://discord.gg/aZWUDjQCbD");
-		}else if(args[1] == "servercount") {
-			message.channel.send(`im doin ${client.guilds.cache.size} moms!`)
-		}else{
-			message.channel.send("Please enter a valid command argument!");
-		}
-		//holy cow thats like, alot of if else statements!
-		//shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut up shut
+		switch(args[1].toLowerCase()) { //holy shit i used something that actually makes sense
+			case "help":
+				message.channel.send(embded);
+				break;
+			case "donate":
+				message.channel.send("thank you for considering donating, in the future ill have names under this command! (once i get people that actually donate :D)\nhttps://www.patreon.com/yamombot");
+				break;
+			case "faq":
+				message.channel.send("no, i am not gonna add a toggle in the next month or two \n what were you expecting");
+				break;
+			case "server":
+				message.channel.send("https://discord.gg/aZWUDjQCbD yamomcord!");
+				break;
+			case "servercount":
+				message.channel.send(`im doin ${client.guilds.cache.size} moms!`);
+				break;
+		} //that felt wayyyyyyy to similar to writing python, i am going to go take a cold shower now
 	}
 
-		//=================
 	//check if they contain trigger word so it doesnt waste time processing looking for verb if it doesnt even matter (hopefully i explained that well)
-	if (!caseYaMomIs.includes(args[0].toLowerCase())) {
-		if (!caseYaMom.includes(args[0].toLowerCase())) {
-			if (!caseVerb.includes(args[0].toLowerCase())) {
-				if (!(args[0] === 'ya')) return; }}}//i took like 5 minutes to write this im gonna have an anrysm that took longer than it shoudlve
-	if (args.length < 2) return; //if message 1 word dont do it or cases dont fit idk man
-	var verbNum;
-	var correctVerbNum = -1;
-	findVerb(args[1]);
-	//WHERE THE MAGIC HAPPENS (choses what case to use) ======================================================================================================================================
-	if (!(correctVerbNum === -1)) { //checks to see if it needs to use gramar
-		if (caseVerb.includes(args[0].toLowerCase())) {
-			message.channel.send(`ya mom ${translatedVerb[correctVerbNum]} ${args.slice(2).join(' ')}`);
-		}
-	} else {
+	if (!(caseYaMomIs.includes(args[0].toLowerCase()) || caseYaMom.includes(args[0].toLowerCase()) || caseVerb.includes(args[0].toLowerCase())) || (args[0] === 'ya')) return;
+	//TRUST ME THIS IS MUCH BETTER COMPARED TO THE LUMP OF CANCER THAT WAS HERE EARLIER
+
+	const verb = findVerb(args[1]); //bro i legit have no idea what to do beside this
+
+	//WHERE THE MAGIC HAPPENS (choses what case to use) =======================
+	if (!(verb == 'undefined') && caseVerb.includes(args[0].toLowerCase())) { //checks to see if it needs to use gramar
+			message.channel.send(`ya mom ${verb} ${args.slice(2).join(' ')}`);
+	} else {//still not putting cases here, simply beacuse they only get indexed 5 and above (i think dont quote me), ill do it next time maybe
+
 		if (args[0].toLowerCase() === 'i' && args[1].toLowerCase() === 'am') {
 			message.channel.send(`ya mom is ${args.slice(2).join(' ')}`);
 			return;
@@ -98,32 +93,66 @@ client.on('message', async (message) => {
 		}
 		if (caseYaMom.includes(args[0].toLowerCase())){
 			message.channel.send(`ya mom ${args.slice(1).join(' ')}`);
-		}
-
-
-	}//===========================================================================================================================================================================================
-//functions that require scope (should be all of them but im just lazy ok?)
-	function findVerb(verb) {
-		for (verbNum = 0; verbNum < baseVerbs.length; verbNum++) {
-			if (verb.toLowerCase() === baseVerbs[verbNum]) {
-				correctVerbNum = verbNum;
-				return;
-			}//repeat loop until found match
-		}
+		} //should've written else if statements :/ class december me!
 	}
-
-});
-
-//these are here because im tooooooo lazy to figure out how to make the status not expire and might as well leave codfe that is used for its unintended purposes
-//i could see no negative reprocussions in the future AT ALL
-client.on("guildCreate", async (guild) => {//when the bot joins a guild
-	refreshStatus();
-});
-client.on("guildDelete", (guild) => { //when the bot joins a guild
-	refreshStatus();
 });
 
 //functions
-function refreshStatus() {
-	client.user.setActivity(`with @ya mom bot#7927 (imposter) | @ya mom bot help`, { type: "PLAYING"});
+function findVerb(verb) {
+	for (let verbNum = 0; verbNum < baseVerbs.length; verbNum++) {
+		if (verb.toLowerCase() === baseVerbs[verbNum]) {
+			return translatedVerb[verbNum];
+		}//repeat loop until found match
+	}
 }
+
+
+//slash command responses
+client.ws.on('INTERACTION_CREATE', async interaction => {
+	switch (interaction.data.name.toLowerCase()) {
+		case "help":
+			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: embded,
+					}
+				}});
+			break;
+
+		case "donate":
+			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: 'thank you for considering donating, in the future ill have names under this command! (once i get people that actually donate :D)\nhttps://www.patreon.com/yamombot'
+					}
+				}})
+			break;
+
+		case "faq":
+			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: 'no, i am not gonna add a toggle in the next month or two \n what were you expecting'
+					}
+				}})
+			break;
+
+		case "server":
+			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: 'https://discord.gg/aZWUDjQCbD yamomcord!'
+					}
+				}})
+			break;
+
+		case "servercount":
+			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: `im doin ${client.guilds.cache.size} moms!`
+					}
+				}})
+			break;
+	}
+})
