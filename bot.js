a00777d052debc227b7b6666816e6f4f10e4bb7a
flopingbird <68748383+flopingbird@users.noreply.github.com>
2021-05-02 19:17:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ client.ws.on('INTERACTION_CREATE', async interaction => {
 			client.api.interactions(interaction.id, interaction.token).callback.post({data: {
 					type: 4,
 					data: {
-						embeds: embded
+						embeds: [embded]
 					}
 				}});
 			break;
